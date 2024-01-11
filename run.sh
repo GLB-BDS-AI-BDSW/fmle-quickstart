133a56cdf738417fd2a4d0bb3901d18a617269b1
Teo Blachere <teo.blachere@eviden.com>
2024-01-11 14:25:21 +0100

    Error handling in run.sh file

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 python ${script_dir}/quickstart.py >> /out/output.txt
+if [[ $? > 0 ]]; then
+   echo "Error"
+   exit 1
+fi
